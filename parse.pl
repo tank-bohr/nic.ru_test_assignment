42a46a43168f0d82975b907fee7c63ffec8f46b2
Alexey Nikitin <bohr@narod.ru>
2012-08-04 14:03:12 +0400

    small improvement

--- parse.pl
+++ parse.pl
@@ -3,7 +3,11 @@
 use DBI;
 use Data::Dumper;
 use FindBin qw/$Bin/;
-use Time::HiRes qw/time/;
+use Time::HiRes qw/
+    time
+    tv_interval
+    gettimeofday
+/;
 
 use strict;
 use warnings;
@@ -17,6 +21,7 @@ unless (-e $file_path) {
 }
 
 
+my $t0 = [gettimeofday()];
 my %pasrsed = (
     message => [],
     log     => []
@@ -32,38 +37,30 @@ if (open my $fh => $file_path) {
 else {
     say "Cannot open file [$file_path]: $!";
 }
+my $parsing_time = tv_interval($t0);
+say "Parsing time: [$parsing_time]";
 
+
+my %fields = (
+    message => [qw/created id int_id str/],
+    log     => [qw/created int_id str address/]
+);
 
 my $dbh = connect_db();
 foreach my $table_name (keys %pasrsed) {
-    #batch_insert($dbh, $table_name, $pasrsed{$table_name});
-    foreach my $item (@{ $pasrsed{$table_name} }) {
-        insert_one($dbh, $table_name, $item);
-    }
+    my $t1 = [gettimeofday()];
+    batch_insert($dbh, {
+        table_name => $table_name,
+        fields => $fields{$table_name},
+        data => $pasrsed{$table_name}
+    });
+    my $elapsed = tv_interval($t1);
+    say "Batch insert to $table_name: [$elapsed]";
 }
 $dbh->disconnect();
 
 
-sub batch_insert {
-    my ($dbh, $table_name, $data) = @_;
 
-    my $fields = $dbh->selectcol_arrayref(qq!SHOW COLUMNS FROM $table_name!);
-    say Dumper($fields);
-
-    my $fields_list  = join ', ' => @$fields;
-    my $placeholder  = join ', ' => map {'?'}              0..$#$fields;
-    my $placeholders = join ', ' => map {"($placeholder)"} 0..$#$data;
-
-    my $query = qq!INSERT INTO $table_name ($fields_list) VALUES $placeholders!;
-
-    my @bind_values;
-    foreach my $item (@$data) {
-        push @bind_values, @{ $item }{ @$fields };
-    }
-
-    say $query;
-    $dbh->do($query, undef, @bind_values);
-}
 
 
 
@@ -91,7 +88,6 @@ sub process_string {
             address => $address
         });
     }
-
 }
 
 
@@ -102,7 +98,7 @@ sub insert_one {
 
     my @fields = grep {defined $item->{$_}} keys %$item;
 
-    my $fields_list = join ', ' => @fields;
+    my $fields_list  = join ', ' => @fields;
     my $placeholders = join ', ' => map {'?'} 0..$#fields;
 
     my $query = qq!INSERT INTO $table_name ($fields_list) VALUES ($placeholders)!;
@@ -112,6 +108,24 @@ sub insert_one {
 }
 
 
+
+sub batch_insert {
+    my ($dbh, $params) = @_;
+    my ($table_name, $fields, $data) = @{ $params }{ qw/table_name fields data/ };
+
+    my $fields_list  = join ', ' => @$fields;
+    my $placeholder  = join ', ' => map {'?'}              0..$#$fields;
+    my $placeholders = join ', ' => map {"($placeholder)"} 0..$#$data;
+
+    my $query = qq!INSERT INTO $table_name ($fields_list) VALUES $placeholders!;
+
+    my @bind_values;
+    foreach my $item (@$data) {
+        push @bind_values, @{ $item }{ @$fields };
+    }
+
+    $dbh->do($query, undef, @bind_values);
+}
 
 
 sub connect_db {
